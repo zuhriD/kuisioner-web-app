9afde361f2b091e8b22b950bc3e035bffe4f259c
zuhriD <dikizuhri@gmail.com>
2023-06-24 16:49:03 +0700

    update view kuisioner

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,6 +4,19 @@
 	<title>Form Tabel</title>
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
 	<style>
+    
+		.logout-button {
+			position: fixed;
+			top: 10px;
+			right: 10px;
+			z-index: 9999;
+		}
+    .save-button {
+			position: fixed;
+			top: 10px;
+			right: 90px;
+			z-index: 9999;
+		}
     .table-bordered {
 			border: 1px solid black;
 		}
@@ -33,11 +46,21 @@ input.form-control {
 	border: 1px solid black;
   border-radius: 0;
 }
+/* style btn */
+.btn-success {
+  background-color: black;
+  border-color: black;
+}
+.btn-success:hover {
+  background-color: black;
+}
 
     
 	</style>
 </head>
 <body>
+  <!-- Tombol Logout -->
+	<a href="#" class="btn btn-danger logout-button">Logout</a>
 	<div class="container">
     {{-- make title here --}}
     <div class="row">
@@ -61,15 +84,17 @@ input.form-control {
 				<tbody>
 					<tr>
 						<td>Provinsi</td>
-						<td>
-              <div class="form-inline">
-                <div class="form-group mr-2">
-                  <input type="text" class="form-control" value="Jawa Timur" >
+            <td>
+              <div class="row">
+                <div class="col-md-10">
+                  <p>Karangrejo</p>
                 </div>
-                <div class="form-group">
-                  <input type="text" class="form-control" value="35" >
-                </div>
-              </div>
+                <div class="col-md-2">
+                    <div class="form-group">
+                      <input type="text" class="form-control" value="018">
+                    </div>
+                  </div>
+              </div>              
             </td>
 						<td>Nama Kepala Keluarga</td>
 						<td><input type="text" class="form-control" value="(Slamet Efendi)"></td>
@@ -77,14 +102,16 @@ input.form-control {
 					<tr>
             <td>Kabupaten</td>
             <td>
-              <div class="form-inline">
-                <div class="form-group mr-2">
-                  <input type="text" class="form-control" value="Banyuwangi" >
+              <div class="row">
+                <div class="col-md-10">
+                  <p>Karangrejo</p>
                 </div>
-                <div class="form-group">
-                  <input type="text" class="form-control" value="10" >
-                </div>
-              </div>
+                <div class="col-md-2">
+                    <div class="form-group">
+                      <input type="text" class="form-control" value="018">
+                    </div>
+                  </div>
+              </div>              
             </td>
             <td>Nomor Urut Bangunan Tempat Tinggal</td>
             <td><input type="text" class="form-control" value="(12)" ></td>
@@ -92,14 +119,16 @@ input.form-control {
           <tr>
             <td>Kecamatan</td>
             <td>
-              <div class="form-inline">
-                <div class="form-group mr-2">
-                  <input type="text" class="form-control" value="Banyuwangi" >
+              <div class="row">
+                <div class="col-md-10">
+                  <p>Karangrejo</p>
                 </div>
-                <div class="form-group">
-                  <input type="text" class="form-control" value="180" >
-                </div>
-              </div>
+                <div class="col-md-2">
+                    <div class="form-group">
+                      <input type="text" class="form-control" value="018">
+                    </div>
+                  </div>
+              </div>              
             </td>
             <td>Nomor Urut Keluarga Hasil Verifikasi</td>
             <td><input type="text" class="form-control" value="(14)" ></td>
@@ -107,23 +136,25 @@ input.form-control {
           <tr>
             <td>Desa/Kelurahan</td>
             <td>
-              <div class="form-inline">
-                <div class="form-group mr-2">
-                  <input type="text" class="form-control" value="Tamanbaru" >
+              <div class="row">
+                <div class="col-md-10">
+                  <p>Karangrejo</p>
                 </div>
-                <div class="form-group">
-                  <input type="text" class="form-control" value="018" >
-                </div>
-              </div>
+                <div class="col-md-2">
+                    <div class="form-group">
+                      <input type="text" class="form-control" value="018">
+                    </div>
+                  </div>
+              </div>              
             </td>
             <td>Status Keluarga</td>
             <td>
               <div class="form-group">
                 <input type="text" class="form-control" value="3">
                 <small class="form-text">
-                  (1) Sangat Miskin<br>
-                  (2) Miskin<br>
-                  (3) Tidak Miskin
+                  1. Sangat Miskin<br>
+                  2. Miskin<br>
+                  3. Tidak Miskin
                 </small>
               </div>
             </td>
@@ -266,21 +297,27 @@ input.form-control {
             </td>
           </tr>
 					<tr>
-            <td colspan="4" class="">
+            <td colspan="2" rowspan="4" class="">
               <div class="row">
-                <div class="col-md-6">
+                <div class="col-md-8">
                   Hasil Pendataan Keluarga
+                  <small class="form-text">
+                    1. Terisi lengkap<br>
+                    2. Terisi tidak lengkap<br>
+                    3. Tidak ada responden yang dapat memberi jawaban sampai akhir masa pendataan<br>
+                    4. Responden menolak<br>
+                    5. Keluarga pindah/bangunan sensus sudah tidak ada
+                  </small>
                 </div>
                 <div class="col-md-2">
                   <input type="text" class="form-control" placeholder="1">
+                 
                 </div>
               </div>
             </td>
-					</tr>
-					<tr>
-						<td colspan="4" class="text-center">
+            <td colspan="2" class="text-center">
               <div class="row">
-                <div class="col-md-6">
+                <div class="col-md-10">
                   Saya menyatakan bahwa informasi yang diberikan adalah benar, dan boleh dipergunakan untuk keperluan pemerintah
                 </div>
                 <div class="col-md-2">
@@ -290,12 +327,12 @@ input.form-control {
             </td>
 					</tr>
 					<tr>
-						<td colspan="4" class="text-center">
+						<td colspan="2" class="text-center">
               <div class="row text-center">
-                <div class="col-md-6">
+                <div class="col-md-8">
                   Nomor Handphone Responden:
                 </div>
-                <div class="col-md-2">
+                <div class="col-md-4">
                   <input type="text" class="form-control" id="nomorHandphone" placeholder="Nomor Handphone">
                 </div>
               </div>
@@ -308,7 +345,7 @@ input.form-control {
     <div class="row mb-2 mt-2">
       <div class="col-md-12">
         <div class="float-right">
-          <button class="btn btn-primary">Simpan</button>
+          <button class="btn btn-success">Kirim</button>
         </div>
       </div>
     </div>
